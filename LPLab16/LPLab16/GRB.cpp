53ed66ca3189ca340d19726b6f3e6d92ff043a9f
garikmogilev <39678226+garikmogilev@users.noreply.github.com>
2020-12-17 22:15:58 +0300

    Update GRB.cpp
    OKAY LET'S GO

--- LPLab16/LPLab16/GRB.cpp
+++ LPLab16/LPLab16/GRB.cpp
@@ -4,87 +4,104 @@
 
 namespace GRB
 {	
-	Rule::Chain::Chain(short psize, GRBALPHABET s, ...)
+	Rule::Chain::Chain(short size, GRBALPHABET s, ...)
 	{
-		nt = new GRBALPHABET[size = psize];
-		int* p = (int*)&s;
-		for (short i = 0; i < psize; i++)
-		{
-			nt[i] = (GRBALPHABET)p[i];
-		}
-	};
+		nt = new GRBALPHABET[this->size = size];
 
-	Rule::Rule(GRBALPHABET pnn, int piderror, short psize, GRB::Rule::Chain c, ...)
+		int* ptr = (int*)& s;
+		for (short i = 0; i < size; i++)
+			nt[i] = (GRBALPHABET)ptr[i];
+	}
+
+	Rule::Rule(GRBALPHABET nn, int iderror, short size, Chain c, ...)
 	{
-		nn = pnn;
-		iderror = piderror;
-		chains = new Chain[size = psize];
-		Chain* p = &c;
-		for (int i = 0; i < size; i++) chains[i] = p[i];
+		this->nn = nn;
+		this->iderror = iderror;
+		this->chains = new Chain[this->size = size];
 
-	};
+		Chain* ptr = &c;
+		for (int i = 0; i < size; i++)
+				this->chains[i] = ptr[i];
+	}
 
-	Greibach::Greibach(GRBALPHABET psrartN, GRBALPHABET pstbottom, short psize, Rule r, ...)
+	Greibach::Greibach(GRBALPHABET startN, GRBALPHABET stbottomT, short size, Rule r, ...)
 	{
-		startN = psrartN;
-		stbottomT = pstbottom;
-		rules = new Rule[size = psize];
-		Rule* p = &r;
-		for (int i = 0; i < size; i++) rules[i] = p[i];
+		this->startN = startN;
+		this->stbottomT = stbottomT;
+		this->rules = new Rule[this->size = size];
+		Rule* ptr = &r;
+		for (int i = 0; i < size; i++)
+			rules[i] = ptr[i];
+	}
 
-	};
-	/*functions and methods gramatic Greibach*/
-	//Greibach getGreibach() { return GRB::greibach; };
-	short Greibach::getRule(GRBALPHABET pnn, Rule& prule)
+	short Greibach::getRule(GRBALPHABET nn, Rule& rule)
 	{
-		short rc = -1;
-		short k = 0;
-		while (k < size && rules[k].nn != pnn) k++;
-		if (k < size) prule = rules[rc = k];
+		short rc = -1, k = 0;
+
+		while (k < size && rules[k].nn != nn)
+			k++;
+
+		if (k < size)
+			rule = rules[rc = k];
+
 		return rc;
-	};
+	}
+
 	Rule Greibach::getRule(short n)
 	{
 		Rule rc;
-		if (n < size) rc = rules[n];
+		if (n < size)
+			rc = rules[n];
 		return rc;
-	};
-	char* Rule::getCRule(char* b, short nchain) // получит привило в виде N->цепочка
+	}
+
+	char* Rule::getCRule(char* b, short nchain)
 	{
-		char bchain[200];
+		char buf[200];
 		b[0] = Chain::alphabet_to_char(nn);
 		b[1] = '-';
 		b[2] = '>';
-		b[2] = 0x00;
-		chains[nchain].getCChain(bchain);
-		strcat_s(b, sizeof(bchain) + 5, bchain);
+		b[3] = 0;
+		chains[nchain].getCChain(buf);
+		strcat_s(b, sizeof(buf) + 5, buf);
+
 		return b;
-	};
-	short Rule::getNextChain(GRBALPHABET t, Rule::Chain& pchain, unsigned short j)
+	}
+
+	short Rule::getNextChain(GRBALPHABET t, Rule::Chain& pchain, short j)
 	{
 		short rc = -1;
-		while (j < size && chains[j].nt[0] != t) ++j;
+			
+		while (j < size && chains[j].nt[0] != t && chains[j].nt[0] != 0)
+			j++;
+
 		rc = (j < size ? j : -1);
-		if (rc >= 0) pchain = chains[rc];
+		if (rc >= 0)
+			pchain = chains[rc];
 		return rc;
-	};
-	char* Rule::Chain::getCChain(char* b)	// получить первую строку правила
+	}
+
+	char* Rule::Chain::getCChain(char* b)
 	{
-		for (int i = 0; i < size; i++)b[i] = Chain::alphabet_to_char(nt[i]);
+		for (int i = 0; i < size; i++)
+			b[i] = Chain::alphabet_to_char(nt[i]);
 		b[size] = 0x00;
 		return b;
 	}
+
+	Greibach getGreibach() { return greibach; }
+}
 #define NS(n) Rule::Chain::N(n)
 #define TS(n) Rule::Chain::T(n)
 
 	Greibach greibach(NS('S'), TS('$'), 6,
 		Rule(NS('S'), GRB_ERROR_SERIES + 0, 
-			2,	//Неверная структура программы
+			2,	//ГЌГҐГўГҐГ°Г­Г Гї Г±ГІГ°ГіГЄГІГіГ°Г  ГЇГ°Г®ГЈГ°Г Г¬Г¬Г»
 			Rule::Chain(7, TS('m'), TS('{'), NS('N'), TS('r'), NS('E'), TS(';'), TS('}')),
 			Rule::Chain(13, TS('t'), TS('f'), TS('i'), TS('('), NS('F'), TS(')'), TS('{'), NS('N'), TS('r'), NS('E'), TS(';'), TS('}'), NS('S'))
 		),
 		Rule(NS('N'), GRB_ERROR_SERIES + 1,
-			9,	//ошибочный оператор
+			9,	//Г®ГёГЁГЎГ®Г·Г­Г»Г© Г®ГЇГҐГ°Г ГІГ®Г°
 			Rule::Chain(4, TS('d'), TS('t'), TS('i'), TS(';')),
 			Rule::Chain(5, TS('d'), TS('t'), TS('i'), TS(';'), NS('N')),
 			Rule::Chain(4, TS('i'), TS('='), NS('E'), TS(';')),
@@ -96,7 +113,7 @@ namespace GRB
 			Rule::Chain(4, TS('p'), NS('E'), TS(';'), NS('N'))
 		),
 		Rule(NS('E'), GRB_ERROR_SERIES + 2, 
-			8,	//выражение
+			8,	//ГўГ»Г°Г Г¦ГҐГ­ГЁГҐ
 			Rule::Chain(1, TS('i')),
 			Rule::Chain(1, TS('l')),
 			Rule::Chain(3, TS('('), NS('E'), TS(')')),
@@ -107,15 +124,15 @@ namespace GRB
 			Rule::Chain(5, TS('i'), TS('('), NS('W'), TS(')'), NS('M'))
 		),
 		Rule(NS('M'), GRB_ERROR_SERIES + 1,
-			2,	//Ошибочный оператор
+			2,	//ГЋГёГЁГЎГ®Г·Г­Г»Г© Г®ГЇГҐГ°Г ГІГ®Г°
 			Rule::Chain(2, TS('v'), NS('E')),
 			Rule::Chain(3, TS('v'), NS('E'), NS('M'))
 		),
-		Rule(NS('F'), GRB_ERROR_SERIES + 3, 2,	// параметры функции при определении
+		Rule(NS('F'), GRB_ERROR_SERIES + 3, 2,	// ГЇГ Г°Г Г¬ГҐГІГ°Г» ГґГіГ­ГЄГ¶ГЁГЁ ГЇГ°ГЁ Г®ГЇГ°ГҐГ¤ГҐГ«ГҐГ­ГЁГЁ
 			Rule::Chain(2, TS('t'), TS('i')),
 			Rule::Chain(4, TS('t'), TS('i'), TS(','), NS('F'))
 		),
-		Rule(NS('W'), GRB_ERROR_SERIES + 4, 4, //параметры вызываемой ф-ции
+		Rule(NS('W'), GRB_ERROR_SERIES + 4, 4, //ГЇГ Г°Г Г¬ГҐГІГ°Г» ГўГ»Г§Г»ГўГ ГҐГ¬Г®Г© Гґ-Г¶ГЁГЁ
 			Rule::Chain(1, TS('i')),
 			Rule::Chain(1, TS('l')),
 			Rule::Chain(3, TS('i'), TS(','), NS('W')),
